165583869d6837e6df6e9895d286f69aadb8a174
Alejandro <alejpr@kth.se>
2023-02-13 10:37:04 +0100

    Robots and paths

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,165 @@ use nannou::prelude::*;
 use std::collections::HashSet;
 
 fn main() {
-    nannou::sketch(view).run();
+    nannou::app(|_| Model {})
+        .update(update)
+        .simple_window(view)
+        .run();
 }
 
+struct Model {}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
 type Node = (i32, i32);
+
+type StdColour = nannou::color::rgb::Rgb<nannou::color::encoding::Srgb, u8>;
+
+const WALL_COLOR: StdColour = BLACK;
+const NODE_COLOR_1: StdColour = CORAL;
+const NODE_COLOR_2: StdColour = CORNFLOWERBLUE;
+const LOOKAHEAD_COLOUR: StdColour = GREEN;
+const ASTAR_COLOUR: StdColour = RED;
 
 struct Warehouse {
     size: (i32, i32),
     walls: HashSet<Node>,
-    drawing: Option<(f32, f32, f32)>,
+}
+
+struct WarehouseSim<'a> {
+    warehouse: &'a Warehouse,
+    position: (f32, f32),
+    cell_size: f32,
+    paths: Vec<AgentPath>,
+    start_time: f32,
+    speed: f32,
+}
+
+struct AgentPath {
+    initial_node: Node,
+    lookahead: Vec<Node>,
+    a_star: Vec<Node>,
 }
 
 impl Warehouse {
     fn new(size: (i32, i32), walls: HashSet<Node>) -> Self {
-        Warehouse {
-            size,
-            walls,
-            drawing: None,
+        Warehouse { size, walls }
+    }
+}
+
+impl<'a> WarehouseSim<'a> {
+    fn new(
+        warehouse: &'a Warehouse,
+        position: (f32, f32),
+        cell_size: f32,
+        paths: Vec<AgentPath>,
+        app: &App,
+        speed: f32,
+    ) -> Self {
+        WarehouseSim {
+            warehouse,
+            position,
+            cell_size,
+            paths,
+            start_time: app.time,
+            speed,
         }
     }
 
-    fn draw(
-        &mut self,
-        topleft_position: (f32, f32),
-        cell_size: f32,
-        app: &App,
-        drawing: &nannou::draw::Draw,
-    ) {
-        self.drawing = Some((topleft_position.0, topleft_position.1, cell_size));
+    fn fnode_to_coord(&self, node: &(f32, f32)) -> (f32, f32) {
+        let base_node = (
+            self.position.0 - self.cell_size * (self.warehouse.size.0 as f32 / 2.0),
+            self.position.1 - self.cell_size * (self.warehouse.size.1 as f32 / 2.0),
+        );
 
-        for i in 0..self.size.1 {
-            for j in 0..self.size.0 {
-                let x = topleft_position.0 + cell_size * (i as f32 + 0.5);
-                let y = topleft_position.1 + cell_size * (j as f32 + 0.5);
-                let color = if self.walls.contains(&(i, j)) {
-                    BLACK
+        (
+            base_node.0 + node.0 * self.cell_size,
+            base_node.1 + node.1 * self.cell_size,
+        )
+    }
+
+    fn inode_to_coord(&self, node: &(i32, i32)) -> (f32, f32) {
+        self.fnode_to_coord(&(node.0 as f32, node.1 as f32))
+    }
+
+    fn draw(&self, drawing: &nannou::draw::Draw, app: &App) {
+        for i in 0..self.warehouse.size.1 {
+            for j in 0..self.warehouse.size.0 {
+                let (x, y) = self.inode_to_coord(&(i, j));
+                let color = if self.warehouse.walls.contains(&(i, j)) {
+                    WALL_COLOR
                 } else if (i + j) % 2 == 0 {
-                    CORAL
+                    NODE_COLOR_1
                 } else {
-                    CORNFLOWERBLUE
+                    NODE_COLOR_2
                 };
                 drawing
                     .rect()
                     .color(color)
                     .x_y(x, y)
-                    .w(cell_size)
-                    .h(cell_size);
+                    .w(self.cell_size)
+                    .h(self.cell_size);
             }
+        }
+
+        for path in self.paths.iter() {
+            self.draw_robot_path(path, app.time, drawing);
         }
     }
 
-    fn draw_robot(&self, path: &[Node], time: f32, drawing: &nannou::draw::Draw) {
-        let diameter = self.drawing.unwrap().2;
-        let current_node = if time >= (path.len() - 1) as f32 {
-            let node = path.iter().last().unwrap();
-            (node.0 as f32, node.1 as f32)
-        } else {
-            let index: usize = time.floor() as usize;
-            let alpha: f32 = time - time.floor();
-            (
-                path[index].0 as f32 * (1.0 - alpha) + path[index + 1].0 as f32 * alpha,
-                path[index].1 as f32 * (1.0 - alpha) + path[index + 1].1 as f32 * alpha,
-            )
-        };
+    fn draw_robot_path(&self, path: &AgentPath, time: f32, drawing: &nannou::draw::Draw) {
+        let diameter = self.cell_size;
 
-        let position = (
-            self.drawing.unwrap().0 + self.drawing.unwrap().2 * (current_node.0 + 0.5),
-            self.drawing.unwrap().1 + self.drawing.unwrap().2 * (current_node.1 + 0.5),
-        );
+        // The current turn, which is the point in the path in which the robot is,
+        // starts at 0 at start_time and increses by "speed" each second.
+        let current_turn = (time/* - self.start_time */) * self.speed;
+
+        // The complete path of the robot is the obatained chaining the 3 parts
+        let mut complete_path = std::iter::once(&path.initial_node)
+            .chain(path.lookahead.iter())
+            .chain(path.a_star.iter());
+
+        let current_node: (f32, f32) =
+            if current_turn.floor() as usize >= complete_path.clone().count() - 1 {
+                // The robot has reached its final destination, so is stays there
+                let inode = complete_path.last().unwrap();
+                (inode.0 as f32, inode.1 as f32)
+            } else {
+                // If that is not the case, we interpolate the position
+                let index = current_turn.floor() as usize;
+                let alpha = current_turn - index as f32;
+                let prev_node = complete_path.nth(index).unwrap();
+                let next_node = complete_path.next().unwrap();
+
+                (
+                    (prev_node.0 as f32) * (1.0 - alpha) + (next_node.0 as f32) * alpha,
+                    (prev_node.1 as f32) * (1.0 - alpha) + (next_node.1 as f32) * alpha,
+                )
+            };
+
+        let mut current_node_in_path = current_node;
+
+        let path_iterator = path
+            .lookahead
+            .iter()
+            .map(|n| (n, LOOKAHEAD_COLOUR))
+            .chain(path.a_star.iter().map(|n| (n, ASTAR_COLOUR)));
+
+        for (node, colour) in path_iterator.skip(current_turn.floor() as usize) {
+            let start = self.fnode_to_coord(&current_node_in_path);
+            let end = self.inode_to_coord(node);
+
+            drawing
+                .line()
+                .start(pt2(start.0, start.1))
+                .end(pt2(end.0, end.1))
+                .weight(self.cell_size / 8.0)
+                .color(colour);
+
+            current_node_in_path = (node.0 as f32, node.1 as f32);
+        }
+
+        let position = self.fnode_to_coord(&current_node);
 
         drawing
             .ellipse()
@@ -79,7 +170,7 @@ impl Warehouse {
     }
 }
 
-fn view(app: &App, frame: Frame) {
+fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLUE);
 
@@ -90,12 +181,20 @@ fn view(app: &App, frame: Frame) {
     walls.insert((2, 3));
     walls.insert((3, 3));
 
-    let path = vec![(0, 0), (0, 1), (1, 1), (1, 3), (1, 4), (2, 4), (5, 4)];
+    let path = AgentPath {
+        initial_node: (0, 0),
+        lookahead: vec![(0, 1), (1, 1), (1, 3)],
+        a_star: vec![(1, 4), (2, 4), (5, 4)],
+    };
 
-    let mut warehouse = Warehouse::new((8, 12), walls);
-    warehouse.draw((-100.0, 0.0), 40.0, app, &draw);
+    let warehouse = Warehouse::new((8, 12), walls);
+    let paths = vec![path];
+
+    let drawhouse = WarehouseSim::new(&warehouse, (0.0, 0.0), 40.0, paths, app, 1.0);
+
+    drawhouse.draw(&draw, app);
+
     let time = app.time;
-    warehouse.draw_robot(&path, time, &draw);
 
     let horizontal = time.sin();
     let vertical = (time * 2.0).sin();
